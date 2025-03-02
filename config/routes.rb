7a8319d70bdaefb81e12ae0529cd1640ce0b605a
david-lafontant <lafontant.david@gmail.com>
2025-03-01 19:15:24 -0500

    Create pages controller with home, about, contact, boutique, and blog actions and related views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+  get "pages/boutique"
+  get "pages/blog"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
