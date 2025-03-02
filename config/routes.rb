667ae013ba7717dc2e799eedb1df9d8c1be08bb2
david-lafontant <lafontant.david@gmail.com>
2025-03-01 19:21:09 -0500

    Customize the routes for the Pages' actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
-  get "pages/boutique"
-  get "pages/blog"
+  root "pages#home"
+  get "about", to: "pages#about", as: :about
+  get "contact", to: "pages#contact", as: :contact
+  get "boutique", to: "pages#boutique", as: :boutique
+  get "blog", to: "pages#blog", as: :blog
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
